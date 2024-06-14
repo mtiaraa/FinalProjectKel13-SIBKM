c7ef6a9fb180b4de28396bc88a7123a3f0dcdb40
Mutiara Rahma <mutiararahmapyb93@gmail.com>
2024-06-15 01:34:36 +0700

    Menambahkan fk pada tabel job histories dan tabel role permissions

--- create job_histories.sql
+++ create job_histories.sql
@@ -7,3 +7,11 @@ job VARCHAR (10),
 department INT,
 PRIMARY KEY (employee, start_date)
 );
+
+-- Menambahkan FK pada employee pada  tbl_job_histories
+ALTER TABLE tbl_job_histories
+ADD CONSTRAINT FK_Employee FOREIGN KEY (employee) REFERENCES tbl_employees(id);
+
+-- Menambahkan FK pada role di tbl_role_permissions
+ALTER TABLE tbl_role_permissions
+ADD CONSTRAINT FK_RolesPermissions FOREIGN KEY (role) REFERENCES tbl_roles(id)
